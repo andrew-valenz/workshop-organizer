8655c3ab08af174f80c35a88efaa8f0ab928f166
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-30 17:25:23 -0800

    added create folder with corresponding files and functions

---
+++ create/create.js
@@ -0,0 +1,32 @@
+import { createParticipant, getWorkshops } from '../fetch-utils.js';
+
+const selectEl = document.querySelector('select');
+const form = document.querySelector('form');
+
+window.addEventListener('load', async () => {
+    const workshops = await getWorkshops();
+
+    for (let workshop of workshops) {
+        const workshopOption = document.createElement('option');
+
+        workshopOption.textContent = workshops.name;
+        workshopOption.value = workshops.id;
+
+        selectEl.append(workshopOption);
+    }
+});
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+    const name = data.get('name');
+    const contact = data.get('contact');
+    const workshopId = data.get('workshop_id');
+
+    await createParticipant({
+        name: name,
+        contact_info: contact,
+        workshop_id: workshopId,
+    });
+});
