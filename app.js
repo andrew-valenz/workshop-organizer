228d71edb48bde18a654b9dc846577eca8d322aa
Ben Greanya <bengreanya@gmail.com>
2022-11-30 16:57:19 -0800

    got tables to render on page and delete working

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* Imports */
-import { getWorkshops, deleteParticipants } from './fetch-utils.js';
+import { getWorkshops, deleteParticipant } from './fetch-utils.js';
 import { renderWorkshops } from './render-utils.js';
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
 
 /* Get DOM Elements */
-const workshopsList = document.querySelector('workshops-list');
+const workshopsList = document.querySelector('.workshops-list');
 /* State */
 
 /* Events */
@@ -13,4 +13,23 @@ window.addEventListener('load', async () => {
     await fetchAndDisplayWorkshops();
 });
 /* Display Functions */
-async function fetchAndDisplayWorkshops() {}
+async function fetchAndDisplayWorkshops() {
+    workshopsList.textContent = '';
+    const shops = await getWorkshops();
+    for (let shop of shops) {
+        const shopEl = renderWorkshops(shop);
+        const participantsEl = document.createElement('ul');
+        for (let participant of shop.participants) {
+            const participantEl = document.createElement('li');
+            participantEl.textContent = `${participant.name}, ${participant.contact_info}`;
+
+            participantEl.addEventListener('click', async () => {
+                await deleteParticipant(participant.id);
+                fetchAndDisplayWorkshops();
+            });
+            participantsEl.append(participantEl);
+        }
+        shopEl.append(participantsEl);
+        workshopsList.append(shopEl);
+    }
+}
